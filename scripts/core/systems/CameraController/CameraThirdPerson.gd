787ac75ce0e6690e6a32c7d6836b78bdf0ae2d91
a6xdev <aa16.principal@gmail.com>
2025-01-03 18:09:50 -0300

    feat: transition between angles dynamically"

--- scripts/core/systems/CameraController/CameraThirdPerson.gd
+++ scripts/core/systems/CameraController/CameraThirdPerson.gd
@@ -1,16 +1,22 @@
 extends Node3D
 
-@onready var spring_arm: SpringArm3D = $SpringArm
-@onready var platform_camera: Camera3D = $PlatformCamera
+@onready var pivot: Node3D = $Pivot
+@onready var spring_arm: SpringArm3D = $Pivot/SpringArm
 
-@export var gameplay_mode:Mode
-enum Mode {
+enum GameplayMode {
 	ThirdPerson,
 	TopDown,
 	Platform
 }
-@export var can_move_mouse:bool = true
 @export var mouse_sens:float = 0.4
+@export_group("Whatever")
+@export var can_move_mouse:bool = true
+
+@export_group("Perspective Positions")
+@export var topDown_angle:Vector3
+@export var platform_angle:Vector3
+
+var pespective_index:int = 0
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -18,5 +24,37 @@ func _ready() -> void:
 func _input(event):
 	if event is InputEventMouseMotion && can_move_mouse:
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
-		spring_arm.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
-		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/3, PI/3) # Limits camera rotation
+		pivot.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		pivot.rotation.x = clamp(pivot.rotation.x, -PI/3, PI/3) # Limits camera rotation
+		
+	if event.is_action_released("change_camera"):
+		pespective_index += 1
+		if pespective_index > (GameplayMode.size() - 1):
+			pespective_index = 0
+
+func _physics_process(delta: float) -> void:
+	print("Pivot: ", pivot.rotation)
+	match pespective_index:
+		GameplayMode.ThirdPerson:
+			can_move_mouse = true
+			pivot.rotation.y = 0
+			pivot.rotation.z = 0
+		GameplayMode.TopDown:
+			can_move_mouse = false
+			transition_pespective(topDown_angle)
+		GameplayMode.Platform:
+			can_move_mouse = false
+			transition_pespective(platform_angle)
+
+func transition_pespective(to:Vector3):
+	var tween = create_tween()
+	
+	pivot.rotation = Vector3.ZERO
+	self.rotation.y = 0
+	
+	tween.tween_property(
+		pivot, "rotation", to, 0.3
+	)
+	
+	Tween.TransitionType.TRANS_SINE
+	Tween.EaseType.EASE_IN_OUT
